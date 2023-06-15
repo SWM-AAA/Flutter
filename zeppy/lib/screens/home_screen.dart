fbcabd98c440965166981f1da6f4e3f22d7d91c6
hexaspace <hexa6do@naver.com>
2023-06-15 19:33:54 +0900

    [TAS-67] Feat: 위치 stream 하고 맵 카메라 이동

--- zeppy/lib/screens/home_screen.dart
+++ zeppy/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -14,14 +15,38 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
-
+  late StreamSubscription<Position> _positionStream;
   late Future<Position> currentLocation = _determinePosition();
+  late LocationSettings locationSettings;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     currentLocation = _determinePosition();
-    // print(currentLocation);
+    locationSettings = _determineLocationSetting();
+    locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 1,
+    );
+
+    _positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position position) {
+      _updateMapCameraPosition(position);
+    });
+  }
+
+  Future<void> _updateMapCameraPosition(Position position) async {
+    final GoogleMapController controller = await _controller.future;
+    LatLng latLng = LatLng(position.latitude, position.longitude);
+    CameraPosition cameraPosition = CameraPosition(target: latLng, zoom: 15);
+    controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    // _updateMarkerPosition(latLng);
+  }
+
+  @override
+  void dispose() {
+    _positionStream.cancel();
+    super.dispose();
   }
 
   static const CameraPosition _kGooglePlex = CameraPosition(
@@ -48,7 +73,12 @@ class _HomeScreenState extends State<HomeScreen> {
               initialCameraPosition: _kGooglePlex,
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
+                // _mapController = controller;
               },
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              compassEnabled: true,
+              mapToolbarEnabled: true,
             ),
           ),
         ],
@@ -60,6 +90,46 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  LocationSettings _determineLocationSetting() {
+    late LocationSettings locationSettings;
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      print("androidandroidandroidandroid");
+      locationSettings = AndroidSettings(
+          accuracy: LocationAccuracy.high,
+          distanceFilter: 1,
+          forceLocationManager: true,
+          intervalDuration: const Duration(seconds: 10),
+          //(Optional) Set foreground notification config to keep the app alive
+          //when going to the background
+          foregroundNotificationConfig: const ForegroundNotificationConfig(
+            notificationText:
+                "Example app will continue to receive your location even when you aren't using it",
+            notificationTitle: "Running in Background",
+            enableWakeLock: true,
+          ));
+    } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.macOS) {
+      print("iOSiOSiOSiOSiOSiOS");
+
+      locationSettings = AppleSettings(
+        accuracy: LocationAccuracy.high,
+        activityType: ActivityType.fitness,
+        distanceFilter: 100,
+        pauseLocationUpdatesAutomatically: true,
+        // Only set to true if our app will be started up in the background.
+        showBackgroundLocationIndicator: false,
+      );
+    } else {
+      print("elseelseelseelseelse");
+
+      locationSettings = const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+      );
+    }
+    return locationSettings;
   }
 
   Future<void> _goToCurrentLocation() async {
