4f133dc37b5c2252d06fdbea2b3064b2251f647d
hexaspace <hexa6do@naver.com>
2023-06-15 18:12:21 +0900

    [TAS-67] Feat: 버튼 누르면 현재 위치로 구글맵 카메라 이동

--- zeppy/lib/main.dart
+++ zeppy/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/current_location_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- zeppy/lib/screens/home_screen.dart
+++ zeppy/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,6 +14,16 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+
+  late Future<Position> currentLocation = _determinePosition();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentLocation = _determinePosition();
+    // print(currentLocation);
+  }
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(
       37.540853,
@@ -20,15 +31,6 @@ class _HomeScreenState extends State<HomeScreen> {
     ),
     zoom: 14.4746,
   );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(
-        37.540853,
-        127.078971,
-      ),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
@@ -49,24 +51,56 @@ class _HomeScreenState extends State<HomeScreen> {
               },
             ),
           ),
-          // IconButton(
-          //   onPressed: _determinePosition,
-          //   iconSize: 120,
-          //   icon: const Icon(Icons.location_on_outlined),
-          // )
         ],
       ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _goToTheLake,
-      //   label: const Text('To the lake!'),
-      //   icon: const Icon(Icons.directions_boat),
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToCurrentLocation,
+        label: const Text('current location!'),
+        icon: const Icon(Icons.location_on_outlined),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToCurrentLocation() async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print('Location: ${position.latitude}, ${position.longitude}');
+
+    CameraPosition kLake = CameraPosition(
+      // bearing: 192.8334901395799,
+      target: LatLng(
+        position.latitude,
+        position.longitude,
+      ),
+      // tilt: 59.440717697143555,
+      zoom: 19.151926040649414,
+    );
+    await controller.animateCamera(CameraUpdate.newCameraPosition(kLake));
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
   }
 }
